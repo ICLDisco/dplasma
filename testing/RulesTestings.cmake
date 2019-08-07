b5177dd7d54f2d8d1cd62051bb3d5b7c292f6bb2
George Bosilca <bosilca@icl.utk.edu>
2019-08-06 23:17:37 -0400

    Correctly detect CUDA support in PaRSEC. Fix the cmake generation of all targets.
    Signed-off-by: George Bosilca <bosilca@icl.utk.edu>
    
    Conflicts:
    	CMakeLists.txt
    	cores/CMakeLists.txt
    	lib/CMakeLists.txt

--- testing/RulesTestings.cmake
+++ testing/RulesTestings.cmake
@@ -12,19 +12,20 @@ macro(testings_addexec OUTPUTLIST PRECISIONS ZSOURCES)
       add_executable(${testings_addexec_EXEC} ${testings_addexec_GENFILE})
       add_dependencies(${testings_addexec_EXEC} dplasma dplasma_includes)
       target_link_libraries(${testings_addexec_EXEC}
-	PRIVATE common dplasma
-	$<$<BOOL:${MPI_C_FOUND}>:MPI::MPI_C>)
+                            PRIVATE common
+                            PUBLIC dplasma
+                            $<$<BOOL:${MPI_C_FOUND}>:MPI::MPI_C>)
     endif(NOT TARGET ${testings_addexec_EXEC})
 
     if( PLASMA_F_COMPILE_SUCCESS )
       set_target_properties(${testings_addexec_EXEC} PROPERTIES
-                              LINKER_LANGUAGE Fortran
-                              COMPILE_FLAGS "${testings_addexec_CFLAGS}"
-                              LINK_FLAGS "${testings_addexec_LDFLAGS} ${LOCAL_FORTRAN_LINK_FLAGS}")
+                            LINKER_LANGUAGE Fortran
+                            COMPILE_FLAGS "${testings_addexec_CFLAGS}"
+                            LINK_FLAGS "${testings_addexec_LDFLAGS} ${LOCAL_FORTRAN_LINK_FLAGS}")
     else( PLASMA_F_COMPILE_SUCCESS )
       set_target_properties(${testings_addexec_EXEC} PROPERTIES
-                              COMPILE_FLAGS "${testings_addexec_CFLAGS}"
-                              LINK_FLAGS "${testings_addexec_LDFLAGS}")
+                            COMPILE_FLAGS "${testings_addexec_CFLAGS}"
+                            LINK_FLAGS "${testings_addexec_LDFLAGS}")
     endif( PLASMA_F_COMPILE_SUCCESS )
 
     list(APPEND ${OUTPUTLIST} ${testings_addexec_EXEC})
