8f2d906a86aed6b93ab446b3a4d853949f669c4f
Aurelien Bouteiller <bouteill@icl.utk.edu>
2019-09-04 00:08:39 -0400

    Copy compile flags from original to precision generated files
    Use CMake functions instead of macros

--- tools/PrecisionGenerator/PrecisionGenerator.cmake
+++ tools/PrecisionGenerator/PrecisionGenerator.cmake
@@ -1,3 +1,14 @@
+# Copyright (c) 2009-2019 The University of Tennessee and The University
+#                         of Tennessee Research Foundation.  All rights
+#                         reserved.
+#
+# $COPYRIGHT$
+#
+# Additional copyrights may follow
+#
+# $HEADER$
+#
+
 #
 # PaRSEC Internal: generation of various floating point precision files from a template.
 #
@@ -29,18 +40,17 @@ endif()
 # "pbasename" where p is the selected precision.
 # the target receives a -DPRECISION_p in its cflags.
 #
-macro(precisions_rules_py)
+function(precisions_rules_py)
   PARSE_ARGUMENTS(PREC_RULE
     "TARGETDIR;PRECISIONS"
     ""
     ${ARGN})
-
-  MESSAGE(STATUS "Generate precision dependencies in ${CMAKE_CURRENT_SOURCE_DIR}")
-
   # The first is the output variable list
   CAR(OUTPUTLIST ${PREC_RULE_DEFAULT_ARGS})
   # Everything else should be source files.
   CDR(SOURCES ${PREC_RULE_DEFAULT_ARGS})
+  MESSAGE(STATUS "Generate precision dependencies in ${CMAKE_CURRENT_SOURCE_DIR}\t${OUTPUTLIST}")
+
   # By default the TARGETDIR is the current binary directory
   if( "${PREC_RULE_TARGETDIR}" STREQUAL "" )
     set(PREC_RULE_TARGETDIR "./")
@@ -99,19 +109,35 @@ macro(precisions_rules_py)
             OUTPUT ${_dependency_OUTPUT}
             COMMAND ${CMAKE_COMMAND} -E remove -f ${_dependency_OUTPUT} && ${pythoncmd} && chmod a-w ${_dependency_OUTPUT}
             DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${_dependency_INPUT} ${PRECISIONPP} ${PRECISIONPP_subs})
-          foreach(_property COMPILE_DEFINITIONS INCLUDE_DIRECTORIES COMPILE_OPTIONS)
-            get_property(_prop_copy SOURCE ${CMAKE_CURRENT_SOURCE_DIR}/${_dependency_INPUT} PROPERTY ${_property})
-            set_source_files_properties(${_dependency_OUTPUT} APPEND PROPERTIES ${_property} "${_prop_copy}")
+          # Copy properties from the original to the generated
+          foreach(_prop_name COMPILE_OPTIONS COMPILE_DEFINITIONS INCLUDE_DIRECTORIES)
+            get_source_file_property(_prop_value ${CMAKE_CURRENT_SOURCE_DIR}/${_dependency_INPUT} ${_prop_name})
+            if( _prop_value )
+              set_property(SOURCE ${_dependency_OUTPUT} APPEND PROPERTY ${_prop_name} "${_prop_value}")
+            endif()
           endforeach()
+
         endif( generate_out )
+        # Remove existing PRECISION_x that may have been copied from the original
+        get_source_file_property(_compile_defs ${_dependency_OUTPUT} COMPILE_DEFINITIONS)
+        if( _compile_defs )
+          foreach(_prec PRECISION_s PRECISION_d PRECISION_c PRECISION_z)
+            list(FIND _compile_defs ${_prec} _index)
+            if( ${_index} GREATER -1 )
+              list(REMOVE_AT _compile_defs ${_index})
+            endif()
+          endforeach()
+          set_source_files_properties(${_dependency_OUTPUT} PROPERTIES COMPILE_DEFINITIONS "${_compile_defs}")
+        endif( _compile_defs )
+        # Add (back?) the PRECISION_x definition
         set_source_files_properties(${_dependency_OUTPUT} APPEND PROPERTIES
             COMPILE_DEFINITIONS "PRECISION_${_dependency_PREC}"
             INCLUDE_DIRECTORIES ${CMAKE_CURRENT_BINARY_DIR})
-        list(APPEND ${OUTPUTLIST} ${_dependency_OUTPUT})
+        list(APPEND outputlist ${_dependency_OUTPUT})
       endif( got_file )
     endif()
   endforeach()
 
-  MESSAGE(STATUS "Generate precision dependencies in ${CMAKE_CURRENT_SOURCE_DIR} - Done")
-
-endmacro(precisions_rules_py)
+  #MESSAGE(STATUS "Generate precision dependencies in ${CMAKE_CURRENT_SOURCE_DIR}\t${OUTPUTLIST} : Done")
+  set(${OUTPUTLIST} ${outputlist} PARENT_SCOPE)
+endfunction(precisions_rules_py)
