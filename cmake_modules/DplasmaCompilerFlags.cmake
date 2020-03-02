54892e91b964b08b2f4762c4bbd8305b20d0fbb1
Aurelien Bouteiller <bouteill@icl.utk.edu>
2020-03-02 14:19:05 -0500

    Import architecture detection from PaRSEC
    Signed-off-by: Aurelien Bouteiller <bouteill@icl.utk.edu>

--- cmake_modules/DplasmaCompilerFlags.cmake
+++ cmake_modules/DplasmaCompilerFlags.cmake
@@ -2,6 +2,66 @@ include (CheckCCompilerFlag)
 include (CheckFortranCompilerFlag)
 
 #
+# Fix the building system for 32 or 64 bits.
+#
+# On MAC OS X there is a easy solution, by setting the
+# CMAKE_OSX_ARCHITECTURES to a subset of the following values:
+# ppc;ppc64;i386;x86_64.
+# On Linux this is a little bit tricky. We have to check that the
+# compiler supports the -m32/-m64 flags as well as the linker.
+# Once this issue is resolved the directory compile_options
+# have to be updated accordingly.
+#
+# TODO: For the Fortran compiler:
+#         no idea how to correctly detect if the required/optional
+#         libraries are in the correct format.
+#
+string(REGEX MATCH ".*xlc$" _match_xlc ${CMAKE_C_COMPILER})
+if(_match_xlc)
+  message(ERROR "Please use the thread-safe version of the xlc compiler (xlc_r)")
+endif(_match_xlc)
+string(REGEX MATCH "XL" _match_xlc ${CMAKE_C_COMPILER_ID})
+if (BUILD_64bits)
+  if( _match_xlc)
+    set( arch_build "-q64" )
+  else (_match_xlc)
+    if( ${CMAKE_SYSTEM_PROCESSOR} STREQUAL "sparc64fx" )
+      set ( arch_build " " )
+    else()
+      set( arch_build "-m64" )
+    endif()
+  endif(_match_xlc)
+else (BUILD_64bits)
+  if( _match_xlc)
+    set( arch_build "-q32" )
+  else (_match_xlc)
+    set( arch_build "-m32" )
+  endif(_match_xlc)
+endif (BUILD_64bits)
+
+check_c_compiler_flag( ${arch_build} C_M32or64 )
+if( C_M32or64 )
+  # Try the same for Fortran and CXX:
+  # Use the same 64bit flag as the C compiler if possible
+  if(CMAKE_Fortran_COMPILER_WORKS)
+    check_fortran_compiler_flag( ${arch_build} F_M32or64 )
+  endif()
+  if(CMAKE_CXX_COMPILER_WORKS)
+    check_cxx_compiler_flag( ${arch_build} CXX_M32or64 )
+  endif()
+  set(arch_build_lang "$<$<COMPILE_LANGUAGE:C>:${arch_build}>")
+  if( F_M32or64 )
+    list(APPEND arch_build_lang "$<$<COMPILE_LANGUAGE:Fortran>:${arch_build}>")
+  endif( F_M32or64 )
+  if( CXX_M32or64 )
+    list(APPEND arch_build_lang "$<$<COMPILE_LANGUAGE:CXX>:${arch_build}>")
+  endif( CXX_M32or64 )
+  set(PARSEC_ARCH_OPTIONS "${arch_build_lang}" CACHE STRING "List of compile options used to select the target architecture (e.g., -m64, -mtune=haswell, etc.)")
+  mark_as_advanced(PARSEC_ARCH_OPTIONS)
+  add_compile_options("${PARSEC_ARCH_OPTIONS}")
+endif( C_M32or64 )
+
+#
 # Check compiler debug flags and capabilities
 #
 
@@ -66,19 +126,41 @@ add_compile_definitions(
 #
 # Fortran tricks: fortran main avoidance
 #
-IF (CMAKE_Fortran_COMPILER_WORKS)
-  STRING(REGEX MATCH "Intel" _match_intel ${CMAKE_Fortran_COMPILER_ID})
-  IF (_match_intel)
-    STRING(APPEND CMAKE_Fortran_LINK_EXECUTABLE " -nofor-main")
-  ENDIF (_match_intel)
+if(CMAKE_Fortran_COMPILER_WORKS)
+  string(REGEX MATCH "Intel" _match_intel ${CMAKE_Fortran_COMPILER_ID})
+  if(_match_intel)
+    string(APPEND CMAKE_Fortran_LINK_EXECUTABLE " -nofor-main")
+  endif(_match_intel)
 
-  STRING(REGEX MATCH "PGI$" _match_pgi ${CMAKE_Fortran_COMPILER_ID})
-  IF (_match_pgi)
-    STRING(APPEND CMAKE_Fortran_LINK_EXECUTABLE " -Mnomain -Bstatic")
-  ENDIF (_match_pgi)
+  string(REGEX MATCH "PGI$" _match_pgi ${CMAKE_Fortran_COMPILER_ID})
+  if(_match_pgi)
+    string(APPEND CMAKE_Fortran_LINK_EXECUTABLE " -Mnomain -Bstatic")
+  endif(_match_pgi)
 
-  STRING(REGEX MATCH ".*xlf$" _match_xlf ${CMAKE_Fortran_COMPILER})
-  IF (_match_xlf)
-    MESSAGE(ERROR "Please use the thread-safe version of the xlf compiler (xlf_r)")
-  ENDIF (_match_xlf)
-ENDIF (CMAKE_Fortran_COMPILER_WORKS)
+  string(REGEX MATCH ".*xlf$" _match_xlf ${CMAKE_Fortran_COMPILER})
+  if(_match_xlf)
+    message(ERROR "Please use the thread-safe version of the xlf compiler (xlf_r)")
+  endif(_match_xlf)
+  
+  get_filename_component (Fortran_COMPILER_NAME ${CMAKE_Fortran_COMPILER} NAME)
+  #  message(STATUS "Fortran Compiler ${Fortran_COMPILER_NAME} id is ${CMAKE_Fortran_COMPILER_ID}")
+  if(${CMAKE_Fortran_COMPILER_ID} STREQUAL "GNU")
+    # gfortran or g77
+    if(${Fortran_COMPILER_NAME} MATCHES g77)
+      add_compile_options("$<$<COMPILE_LANGUAGE:Fortran>:-fno-f2c>")
+    endif()
+    # We append the implicit fortran link flags for the case where FC=/somepath/f90
+    # and /somepath/lib/libf90.so is not in LD_LIBRARY_PATH. This is typical for non-system
+    # installed gfortan where the implicit -lgfortran may not resolved at application link time
+    # otherwise.
+    foreach(item IN ITEMS ${CMAKE_Fortran_IMPLICIT_LINK_DIRECTORIES})
+      list(APPEND EXTRA_LIBS "-L${item}")
+    endforeach()
+    list(APPEND EXTRA_LIBS ${CMAKE_Fortran_IMPLICIT_LINK_LIBRARIES})
+  elseif(${CMAKE_Fortran_COMPILER_ID} STREQUAL "Intel")
+    # ifort
+    add_compile_options("$<$<COMPILE_LANGUAGE:Fortran>:-f77rtl>")
+    # This is a bug in CMake, which incorrectly adds this flag that does not exist on some ifort versions.
+    string (REPLACE "-i_dynamic" "" CMAKE_SHARED_LIBRARY_LINK_Fortran_FLAGS "${CMAKE_SHARED_LIBRARY_LINK_Fortran_FLAGS}")
+  endif (${CMAKE_Fortran_COMPILER_ID} STREQUAL "GNU")
+endif(CMAKE_Fortran_COMPILER_WORKS)
