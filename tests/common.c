d9d8a18ddc64dff65cf71902d8ce5cb841ab0466
Aurelien Bouteiller <bouteill@icl.utk.edu>
2020-10-19 15:51:12 -0400

    hip: Configury
    Signed-off-by: Aurelien Bouteiller <bouteill@icl.utk.edu>

--- tests/common.c
+++ tests/common.c
@@ -314,7 +314,7 @@ static void read_arguments(int *_argc, char*** _argv, int* iparam)
                 break;
 
             case 'g':
-#if !defined(DPLASMA_HAVE_CUDA)
+#if !defined(DPLASMA_HAVE_CUDA) && !defined(DPLASMA_HAVE_HIP)
                 iparam[IPARAM_NGPUS] = DPLASMA_ERR_NOT_SUPPORTED; /* force an error message */
 #endif
                 if(iparam[IPARAM_NGPUS] == DPLASMA_ERR_NOT_SUPPORTED) {
@@ -326,6 +326,7 @@ static void read_arguments(int *_argc, char*** _argv, int* iparam)
 
                 rc = asprintf(&value, "%d", iparam[IPARAM_NGPUS]);
                 parsec_setenv_mca_param( "device_cuda_enabled", value, &environ );
+                parsec_setenv_mca_param( "device_hip_enabled", value, &environ );
                 free(value);
                 break;
 
