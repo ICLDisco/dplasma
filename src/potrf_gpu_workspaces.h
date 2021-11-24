8d88461d030bdd1dcedbef67ebade227360f3230
Aurelien Bouteiller <bouteill@icl.utk.edu>
2021-11-24 12:28:57 -0500

    hip: rework of PO and workspaces
    Signed-off-by: Aurelien Bouteiller <bouteill@icl.utk.edu>

---
+++ src/potrf_gpu_workspaces.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2020-2021 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ *
+ */
+#ifndef DPLASMA_POTRF_GPU_WORKSPACES_H
+#define DPLASMA_POTRF_GPU_WORKSPACES_H
+
+typedef struct {
+  char         *tmpmem;
+  void         *memory;
+  int           lwork;
+} dplasma_potrf_workspace_t;
+
+#endif //DPLASMA_POTRF_GPU_WORKSPACES_H
